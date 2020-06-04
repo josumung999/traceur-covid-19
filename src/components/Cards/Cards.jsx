d996d2e1989c42b521b81b690ae7ae9d8935d394
Joshua <jmunganga039@gmail.com>
2020-06-04 18:46:28 +0200

    Started Working on Global Chart

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
+import cx from 'classnames';
 
-const Cards = () => {
+import styles from './Cards.module.css';
+
+const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
+	if(!confirmed) {
+		return 'Chargement ... ';
+	}
+
 	return (
-		<h1>Cards</h1>
-	)
+		<div className={ styles.container }>
+			<Grid container spacing={3} justify="center">
+					<Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
+						<CardContent>
+							<Typography color="textSecondary" gutterBottom>Personnes Infectées</Typography>
+							<Typography variant="h5">
+								<CountUp start={0} end={ confirmed.value } duration={4} separator="." />
+							</Typography>
+							<Typography color="textSecondary" gutterBottom>{ new Date(lastUpdate).toDateString() }</Typography>
+							<Typography variant="body2">Nombre de cas actifs du Covid-19</Typography>
+						</CardContent>
+					</Grid>
+
+					<Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
+						<CardContent>
+							<Typography color="textSecondary" gutterBottom>Guerries</Typography>
+							<Typography variant="h5">
+								<CountUp start={0} end={ recovered.value } duration={4} separator="." />
+							</Typography>
+							<Typography color="textSecondary" gutterBottom>{ new Date(lastUpdate).toDateString() }</Typography>							<Typography variant="body2">Nombre de personnes guerries du Covid-19</Typography>
+						</CardContent>
+					</Grid>
+
+					<Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
+						<CardContent>
+							<Typography color="textSecondary" gutterBottom>Décès</Typography>
+							<Typography variant="h5">
+								<CountUp start={0} end={ deaths.value } duration={4} separator="." />
+							</Typography>
+							<Typography color="textSecondary" gutterBottom>{ new Date(lastUpdate).toDateString() }</Typography>
+							<Typography variant="body2">Nombre de personnes décedées suite au Covid-19</Typography>
+						</CardContent>
+					</Grid>
+			</Grid>
+		</div>
+	);
 }
 
-export default Cards;
+export default Cards
